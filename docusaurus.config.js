95aca3db2250516712a78bc11fec7b6b3b4c26bc
dv <dmytro4vynogradov@gmail.com>
2023-03-01 07:05:19 +0200

    Modified the site titles

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Docs-Site-Docs',
-  tagline: 'Documentation is cool',
+  tagline: 'Docs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'To the Docs!',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
